cd077539c792a41add428a0aea1038fb690dd804
jhagspiel <j.hagspiel@student.tudelft.nl>
2019-04-25 17:35:31 +0200

    Fixed formatting to comply with checkstyle

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -5,21 +5,27 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * The type Board test.
+ */
 public class BoardTest {
+
     private Square square;
 
+    /**
+     * Creates a new BasicSquare for each test method .
+     */
     @BeforeEach
-    public void setup()
-    {
+    public void setup() {
         square = new BasicSquare();
     }
 
-
+    /**
+     * Tests board creation with a valid grid.
+     */
     @Test
-    public void testBoard()
-    {
+    public void testBoard() {
         Board test = new Board(new Square[][] {{ square }});
-        assertThat(test.squareAt(0,0)).isEqualTo(square);
-
+        assertThat(test.squareAt(0, 0)).isEqualTo(square);
     }
 }
