b5ae9ffce6b1f5cc5cfa20f9f997c3e5f46144b2
jhagspiel <j.hagspiel@student.tudelft.nl>
2019-04-25 17:19:50 +0200

    Added board tests to check assertions

---
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -0,0 +1,32 @@
+package nl.tudelft.jpacman.board;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BoardTest {
+    private Square square;
+
+    @BeforeEach
+    public void setup()
+    {
+        square = new BasicSquare();
+    }
+
+
+    @Test
+    public void testBoard()
+    {
+        Board test = new Board(new Square[][] {{ square }});
+        assertThat(test.squareAt(0,0)).isEqualTo(square);
+
+    }
+
+    @Test
+    public void testBoardNull()
+    {
+        Board test = new Board(new Square[][] {{ null }});
+        assertThat(test.squareAt(0,0)).isEqualTo(null);
+    }
+}
